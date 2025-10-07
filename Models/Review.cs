3685a1f4ebd4ee9088cbd2cb869a2ed97e17a97b
Cristian Radulescu <kristianradulescu@gmail.com>
2025-10-07 11:35:04 +0200

    chore: added one to many and many to many relationships

--- Models/Review.cs
+++ Models/Review.cs
@@ -5,4 +5,7 @@ public class Review
     public int Id { get; set; }
     public string Title { get; set; }
     public string Text { get; set; }
+    public ICollection<Review> Reviews { get; set; }
+    public Reviewer Reviewer { get; set; }
+    public Pokemon Pokemon { get; set; }
 }
