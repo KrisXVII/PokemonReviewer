166f7713bad06c19186ac16fc72d0e8c3f0740bc
Cristian Radulescu <kristianradulescu@gmail.com>
2025-10-08 15:33:13 +0200

    fix: made foreign key not null

--- Migrations/20251008133026_MakePokemonRequiredInReview.Designer.cs
+++ Migrations/20251008133026_MakePokemonRequiredInReview.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PokemonReviewer.Data;
 
@@ -11,9 +12,11 @@ using PokemonReviewer.Data;
 namespace PokemonReviewer.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20251008133026_MakePokemonRequiredInReview")]
+    partial class MakePokemonRequiredInReview
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20251008133026_MakePokemonRequiredInReview.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PokemonReviewer.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakePokemonRequiredInReview : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reviews_Pokemon_PokemonId",
+                table: "Reviews");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PokemonId",
+                table: "Reviews",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_Pokemon_PokemonId",
+                table: "Reviews",
+                column: "PokemonId",
+                principalTable: "Pokemon",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reviews_Pokemon_PokemonId",
+                table: "Reviews");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PokemonId",
+                table: "Reviews",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_Pokemon_PokemonId",
+                table: "Reviews",
+                column: "PokemonId",
+                principalTable: "Pokemon",
+                principalColumn: "Id");
+        }
+    }
+}
